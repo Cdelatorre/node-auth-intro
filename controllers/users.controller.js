6cb1e4fe8e34eaa1db475aa2592f3fb0ce75fb87
Carlos de la Torre Díaz <carlosdelatorrediaz@gmail.com>
2024-04-13 13:41:43 +0200

    feat: add logout behavior

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -92,3 +92,8 @@ module.exports.doRegister = (req, res, next) => {
       }
     });
 };
+
+module.exports.logout = (req, res, next) => {
+  req.session.destroy();
+  res.redirect("/users/login");
+};
